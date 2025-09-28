f28f75ff7af53159ac70816bb4f8939a0ba48fc3
Andka670 <m.andhikaa2018@gmail.com>
2025-09-28 16:41:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ with st.sidebar:
 # ----------------------------
 st.markdown(""" <style>
 div[data-testid="stButton"] > button {
-    width: 100%;
+    width: 1000px;
     min-height: 50px;
     padding: 15px 0;
     border-radius: 20px;
