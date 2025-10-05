cf3cbea326920066acf37b480084de240fff58f4
Andka670 <m.andhikaa2018@gmail.com>
2025-10-05 22:55:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -360,7 +360,7 @@ else:
 # =====================================================
 # Halaman Profil
 # =====================================================
-elif st.session_state.page == "profil":
+if st.session_state.page == "profil":
     st.title("⚙️ Profil")
     st.markdown(f"<p class='profil-text'>👤 Username: {user['username']}</p>", unsafe_allow_html=True)
     st.markdown(f"<p class='profil-text'>🆔 ID User: {user['id_user']}</p>", unsafe_allow_html=True)
